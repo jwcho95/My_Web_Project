acbfc4cc64a962fa5fda1ae2219c6b24fe7ac867
jwcho95 <kyaleee@naver.com>
2023-05-22 17:23:36 +0900

    2023/05/22

---
+++ RandingPageProject/프로젝트 최종 수정본/categories.js
@@ -0,0 +1,74 @@
+const getShow = function() {
+    var con = document.getElementById("close")
+
+    if(con.style.display =="none") {
+            con.style.display = "block";
+            document.querySelector(".btn").style = 
+            "transform : rotate(0deg);"
+    }
+    else {
+            con.style.display = "none";
+            document.querySelector(".btn").style = 
+            "transform : rotate(-180deg);"
+    }
+}
+
+const getShow2 = function() {
+        var con = document.getElementById("close_2")
+    
+        if(con.style.display =="none") {
+                con.style.display = "block";
+                document.querySelector(".btn2").style = 
+                "transform : rotate(0deg);"
+        }
+        else {
+                con.style.display = "none";
+                document.querySelector(".btn2").style = 
+                "transform : rotate(-180deg);"
+        }
+    }
+
+    const getShow3 = function() {
+        var con = document.getElementById("close_3")
+    
+        if(con.style.display =="none") {
+                con.style.display = "block";
+                document.querySelector(".btn3").style = 
+                "transform : rotate(0deg);"
+        }
+        else {
+                con.style.display = "none";
+                document.querySelector(".btn3").style = 
+                "transform : rotate(-180deg);"
+        }
+    }
+
+    const getShow4 = function() {
+        var con = document.getElementById("close_4")
+    
+        if(con.style.display =="none") {
+                con.style.display = "block";
+                document.querySelector(".btn4").style = 
+                "transform : rotate(0deg);"
+        }
+        else {
+                con.style.display = "none";
+                document.querySelector(".btn4").style = 
+                "transform : rotate(-180deg);"
+        }
+    }
+
+    const getShow5 = function() {
+        var con = document.getElementById("close_5")
+    
+        if(con.style.display =="none") {
+                con.style.display = "block";
+                document.querySelector(".btn5").style = 
+                "transform : rotate(0deg);"
+        }
+        else {
+                con.style.display = "none";
+                document.querySelector(".btn5").style = 
+                "transform : rotate(-180deg);"
+        }
+    }
